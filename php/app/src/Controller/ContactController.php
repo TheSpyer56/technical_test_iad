46b6bc104339c6f5e97578c9f6014be818f7504c
Julien Mertian de Muller <julien.mertian-de-muller@epitech.eu>
2022-04-26 20:51:20 +0200

    route: new route update_contact allow to modify an existing contact in database

--- php/app/src/Controller/ContactController.php
+++ php/app/src/Controller/ContactController.php
@@ -18,6 +18,7 @@ class ContactController extends AbstractController
     {
         $parameter = json_decode($request->getContent(), true);
         $contact = new Contact(...$parameter);
+
         $this->insertOnDb($contact);
         return $this->json('Inserted Successfully', 201);
     }
@@ -25,15 +26,29 @@ class ContactController extends AbstractController
     #[Route('/contact/{id}', name: 'read_contact', methods: 'GET')]
     public function read_contact($id): Response {
         $data = $this->findOneContactById($id);
+
         if (!$data)
             return $this->json("No contact exist on the database with the id: ".$id, 404);
         $res = $this->fromDoctrineToArray($data);
         return $this->json($res, 200);
     }
+
+    #[Route('/contact/{id}', name: 'update_contact', methods: 'PUT')]
+    public function update_contact(Request $request, $id): Response {
+        $parameter = json_decode($request->getContent(), true);
+        $contact = new Contact(...$parameter);
+        $data = $this->findOneContactById($id);
+        if (!$data)
+            return $this->json("No contact exist on the database with the id: ".$id, 404);
+        $this->updateContactWithReqParams($data, $parameter);
+        $this->insertOnDb($data);
+        return $this->json('Updated Successfully', 200);
+    }
     
     #[Route('/contact/{id}', name: 'delete_contact', methods: 'DELETE')]
     public function delete_contact($id): Response {
         $data = $this->findOneContactById($id);
+
         if (!$data)
             return $this->json("No contact exist on the database with the id: ".$id, 404);
         $this->deleteOnDb($data);
@@ -43,6 +58,7 @@ class ContactController extends AbstractController
     #[Route('/contact', name: 'read_all_contact', methods: 'GET')]
     public function read_all_contact(): Response {
         $data = $this->getAllContact();
+
         if (!$data)
             return $this->json('Contact database is empty !', 404);
         foreach ($data as $d) {
@@ -54,22 +70,35 @@ class ContactController extends AbstractController
 
     private function findOneContactById($id) {
         $data = $this->doctrine->getRepository(Contact::class)->find($id);
+
         return $data;
     }
 
     private function getAllContact() {
         $data = $this->doctrine->getRepository(Contact::class)->findAll();
+
         return $data;
+    }
+
+    private function updateContactWithReqParams(&$data, $parameters) {
+        $data->setName($parameters['name']);
+        $data->setSurname($parameters['surname']);
+        $data->setEmail($parameters['email']);
+        $data->setAddress($parameters['address']);
+        $data->setPhone($parameters['phone']);
+        $data->setAge($parameters['age']);
     }
 
     private function insertOnDb($data) {
         $em = $this->doctrine->getManager();
+
         $em->persist($data);
         $em->flush();
     }
 
     private function deleteOnDb($data) {
         $em = $this->doctrine->getManager();
+
         $em->remove($data);
         $em->flush();
     }
